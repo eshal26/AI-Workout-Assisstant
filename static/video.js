77e55c3550799327b86c9ef8ef25aa4da450893d
Eshal <eshalali68@gmail.com>
2024-08-08 20:40:42 +0500

    updated-ui

--- static/video.js
+++ static/video.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const videoFeed = document.getElementById('videoFeed');
     const urlParams = new URLSearchParams(window.location.search);
     const exerciseId = urlParams.get('exercise_id');
-    const exerciseHeader = document.getElementById('exercise');
     const resetButton= document.getElementById('resetButton');
 
     resetButton.addEventListener('click', () => {
@@ -20,14 +19,6 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Error:', error);
     });
 });
-
-    if (exerciseId == 1)
-    exerciseHeader.textContent = `Squat`;
-    else if (exerciseId == 2)
-    exerciseHeader.textContent = `Bicep Curl`;
-    else if (exerciseId == 3)
-    exerciseHeader.textContent = `Sit-up`;
-
 
     if (exerciseId && videoFeed) {
         videoFeed.src = `/video_feed/${exerciseId}`;
